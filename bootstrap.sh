d550b00038e78aae0ebb73b8491aaac233701dd7
Dov Murik <dov.murik@gmail.com>
2016-03-08 15:09:52 -0500

    Bootstrap malc!

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+log() {
+  echo "boostrap.sh: $@"
+}
+
+if [ ! -f ./malc ] ; then
+  echo "Must run bootstrap.sh from the malc project directory."
+  exit 1
+fi
+
+if [ -z "$MAL_IMPL" ] ; then
+  log "\$MAL_IMPL is not set; using built-in default Mal interpreter"
+  export MAL_PREFIX="ruby"
+  export MAL_IMPL="./mal-interpreter/ruby/stepA_mal.rb"
+fi
+
+log "Compiling mal-to-llvm.mal ..."
+./malc -v -c mal-to-llvm.mal
+
+log "Done"
