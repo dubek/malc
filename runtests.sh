84196cee33e1db6c326abcc55c5fa48b1c676ebd
Dov Murik <dov.murik@gmail.com>
2016-03-09 11:27:38 -0500

    tests: run all tests and count failures

--- runtests.sh
+++ runtests.sh
@@ -9,32 +9,44 @@ test_one() {
   ./malc -v -l -c $testfile -o $exe > tests/tmp/malc.log 2>&1
   if [[ $? != 0 ]] ; then
     echo "ERROR compiling $testfile; log is in tests/tmp/malc.log"
-    exit 1
+    return 1
   fi
   echo -n "Running ... "
   $exe > tests/tmp/test_output
   if [[ $? != 0 ]] ; then
     echo "ERROR running $testfile: test output:"
     cat tests/tmp/test_output
-    exit 1
+    return 1
   fi
   sed -ne 's/^;; *EXPECTED: *//p' $testfile > tests/tmp/expected_output
   diff -q tests/tmp/expected_output tests/tmp/test_output > /dev/null
   if [[ $? != 0 ]] ; then
     echo "FAIL"
     diff -u --label expected_output tests/tmp/expected_output --label test_output tests/tmp/test_output
-    exit 1
+    return 1
   fi
   rm -rf tests/tmp
   echo "PASS"
+  return 0
 }
 
 if [ -z "$1" ] ; then
-  for testfile in tests/*.mal ; do
-    test_one $testfile
-  done
+  alltests="tests/*.mal"
 else
-  test_one $1
+  alltests=($1)
 fi
 
-echo "Success"
+tests=0
+failures=0
+for testfile in $alltests ; do
+  tests=$((tests+1))
+  test_one $testfile
+  failures=$((failures+$?))
+done
+
+if [ $failures = 0 ] ; then
+  echo "Success, $tests tests OK"
+else
+  echo "Done, $failures tests failed/errored (out of $tests tests)"
+  exit 1
+fi
